45938500d82ab284bdb09b43508ee052c203e88a
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2025-02-14 12:21:14 +0100

    Suppression test inutile (5 passed)

--- src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Assertions;
 
 import java.util.concurrent.CompletableFuture;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Description du contenu : Cette classe va nous servir à définir tous les tests
  * nécessaires au bon fonctionnement de notre jeu
@@ -16,7 +18,6 @@ import java.util.concurrent.CompletableFuture;
  * - Si la défense du premier pokemon est supérieure et que l'attaque des deux pokemons est la meme, il est vainqueur
  * - Si la défense du premier pokemon est inférieure et que l'attaque des deux pokemons est la meme, il est perdant
  * - Si les statistiques d'attaque et de défense sont les mêmes, le premier pokemon est vainqueur
- * - Si les statistiques d'attaque et de défense sont les mêmes, le deuxième pokemon est perdant
  */
 
 class PokemonTest {
@@ -35,7 +36,7 @@ class PokemonTest {
         boolean vainqueur = pokemon1.estVainqueurContre(pokemon2);
 
         //WHEN
-        Assertions.assertEquals(false, vainqueur);
+        assertThat(vainqueur).isFalse();
     }
 
     @Test
@@ -52,7 +53,7 @@ class PokemonTest {
         boolean vainqueur = pokemon1.estVainqueurContre(pokemon2);
 
         //WHEN
-        Assertions.assertEquals(true, vainqueur);
+        assertThat(vainqueur).isTrue();
     }
 
     @Test
@@ -65,7 +66,7 @@ class PokemonTest {
         boolean vainqueur = pokemon1.estVainqueurContre(pokemon2);
 
         //WHEN
-        Assertions.assertEquals(true, vainqueur);
+        assertThat(vainqueur).isTrue();
     }
 
     @Test
@@ -78,7 +79,7 @@ class PokemonTest {
         boolean vainqueur = pokemon1.estVainqueurContre(pokemon2);
 
         //WHEN
-        Assertions.assertEquals(false, vainqueur);
+        assertThat(vainqueur).isFalse();
     }
 
     @Test
@@ -91,19 +92,6 @@ class PokemonTest {
         boolean vainqueur = pokemon1.estVainqueurContre(pokemon2);
 
         //WHEN
-        Assertions.assertEquals(true, vainqueur);
-    }
-
-    @Test
-    void l_attaque_et_la_defense_sont_les_memes_deuxieme_perdant() {
-        //GIVEN
-        Pokemon pokemon1 = new Pokemon("p1", "url", new Stats(10, 12));
-        Pokemon pokemon2 = new Pokemon("p2", "url", new Stats(10, 12));
-
-        //WHEN
-        boolean vainqueur = pokemon2.estVainqueurContre(pokemon1);
-
-        //WHEN
-        Assertions.assertEquals(false, vainqueur);
+        assertThat(vainqueur).isTrue();
     }
 }
